da12473f2421985e0a680941af5f22e296dc36d3
HyunJun Jeong <rollrat.cse@gmail.com>
2023-03-10 22:55:09 +0900

    Update hooks-backend-ci.sh

--- hooks-backend-ci.sh
+++ hooks-backend-ci.sh
@@ -4,18 +4,13 @@ docker stop todobackend && docker rm $_
 docker rmi rollrat/todo-backend:latest
 docker pull rollrat/todo-backend:latest
 
-# download env-file for docker
-aws s3api get-object --bucket todo-config --key .env.docker.prod .env.docker.prod
-
 # setup docker
-docker create --env-file ./.env.docker.prod \
-                  --name todobackend \
-                  --network host \
-                  --add-host host.docker.internal:host-gateway \
-                  rollrat/todo-backend:latest
+docker create --name todobackend \
+              --network host \
+              rollrat/todo-backend:latest
 
 # download env & copy to container
-aws s3api get-object --bucket todo-config --key .prod.env .prod.env
+aws s3api get-object --bucket todo-config --key .env.docker.prod .env
 docker cp .prod.env todobackend:/home/node/
 
 # run docker
@@ -23,4 +18,3 @@ docker start todobackend
 docker rmi $(docker images -q rollrat/todo-backend)
 
 rm .env.docker.prod
-rm .prod.env
