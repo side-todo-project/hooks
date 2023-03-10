80bef19b07fef49221976c587f504f7d0775dca2
HyunJun Jeong <rollrat.cse@gmail.com>
2023-03-10 22:21:41 +0900

    Create hooks-backend-ci.sh

---
+++ hooks-backend-ci.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# sync docker
+docker stop todobackend && docker rm $_
+docker rmi rollrat/todo-backend:latest
+docker pull rollrat/todo-backend:latest
+
+# download env-file for docker
+aws s3api get-object --bucket todo-backend-config --key .env.docker.prod .env.docker.prod
+
+# setup docker
+docker create --env-file ./.env.docker.prod \
+                  --name todobackend \
+                  --network host \
+                  --add-host host.docker.internal:host-gateway \
+                  rollrat/todo-backend:latest
+
+# download env & copy to container
+aws s3api get-object --bucket todo-config --key .prod.env .prod.env
+docker cp .prod.env todobackend:/home/node/
+
+# run docker
+docker start todobackend
+docker rmi $(docker images -q rollrat/todo-backend)
+
+rm .env.docker.prod
+rm .prod.env
