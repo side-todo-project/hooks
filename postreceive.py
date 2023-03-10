6f4651fca136a5a8844acb770d03422a8e998048
HyunJun Jeong <rollrat.cse@gmail.com>
2023-03-10 22:19:39 +0900

    Create postreceive.py

---
+++ postreceive.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/postreceive_frontend', methods=['POST'])
+def back():
+    os.system("./hooks-frontend-ci.sh")
+    return 'Success!'
+
+
+@app.route('/postreceive_backend', methods=['POST'])
+def back():
+    os.system("./hooks-backend-ci.sh")
+    return 'Success!'
+
+
+if __name__ == "__main__":
+    app.run()
